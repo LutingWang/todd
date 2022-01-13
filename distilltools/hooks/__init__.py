9cecd39394d679b209ffed51d2658d5f4b33dab8
LutingWang <2457348692@qq.com>
2022-01-13 16:11:20 +0800

    fix: hooks.__init__

--- distilltools/hooks/__init__.py
+++ distilltools/hooks/__init__.py
@@ -1,6 +1,5 @@
 from .base import BaseHook
-from .builder import HOOKS
-from .modules import HookModule, TrackingModule
+from .builder import HOOKS, HookModule, TrackingModule
 from .multi_calls import MultiCallsHook
 from .multi_tensors import MultiTensorsHook
 from .standard import StandardHook
