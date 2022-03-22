f6255ed471ed046ad2e7af65aa0c21959f21119a
LutingWang <wangluting.wlt@alibaba-inc.com>
2022-03-22 15:27:27 +0800

    feat: compat python3.6 for missing cached_properties

--- todd/__init__.py
+++ todd/__init__.py
@@ -1,6 +1,22 @@
 try:
     from functools import cached_property
 except ImportError:
+
+    class cached_property:
+        def __init__(self, func):
+            self.func = func
+            self.__doc__ = func.__doc__
+    
+        def __set_name__(self, owner, name):
+            self.attrname = name
+    
+        def __get__(self, instance, owner=None):
+            if self.attrname in instance.__dict__:
+                return instance.__dict__[self.attrname]
+            val = self.func(instance)
+            cache[self.attrname] = val
+            return val
+
     import functools
     functools.cached_property = property
 
