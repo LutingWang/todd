faa45f5f8994468177888e7388ed8bd9620a221f
LutingWang <wangluting.wlt@alibaba-inc.com>
2022-06-13 13:29:16 +0800

    fix: optionally patch builtins.breakpoint

--- todd/_patches/builtins.py
+++ todd/_patches/builtins.py
@@ -1,5 +1,13 @@
 import builtins
 
-import ipdb
+from .logging import get_logger
 
-builtins.breakpoint = ipdb.set_trace
+_logger = get_logger()
+
+try:
+    import ipdb
+
+    _logger.info("`ipdb` is installed. Using it for debugging.")
+    builtins.breakpoint = ipdb.set_trace
+except ImportError:
+    pass
