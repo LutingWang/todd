b1572931efbcbbbb1885db112b693b6e8e7faa59
LutingWang <wangluting.wlt@alibaba-inc.com>
2022-08-15 17:16:07 +0800

    feat(adapts): register all adaptations

--- todd/adapts/base.py
+++ todd/adapts/base.py
@@ -3,7 +3,10 @@ __all__ = [
     'ADAPTS',
 ]
 
-import einops.layers.torch as einops
+import inspect
+import itertools
+
+import einops.layers.torch
 import torch.nn as nn
 
 from ..base import STEPS, Module, Registry
@@ -14,16 +17,20 @@ class BaseAdapt(Module):
 
 
 ADAPTS: Registry[nn.Module] = Registry('adapts', parent=STEPS, base=nn.Module)
-ADAPTS.register(nn.Linear)
-ADAPTS.register(einops.Rearrange)
-ADAPTS.register(einops.Reduce)
+for _, class_ in itertools.chain(
+    inspect.getmembers(nn, inspect.isclass),
+    inspect.getmembers(einops.layers.torch, inspect.isclass),
+):
+    if issubclass(class_, nn.Module):
+        ADAPTS.register(class_)
 
 try:
-    from mmcv.cnn import CONV_LAYERS, PLUGIN_LAYERS
+    import mmcv.cnn
 
-    for k, v in PLUGIN_LAYERS.module_dict.items():
-        ADAPTS.register(v, name=k)
-    for k, v in CONV_LAYERS.module_dict.items():
-        ADAPTS.register(v, name=k)
+    for k, v in itertools.chain(
+        mmcv.cnn.CONV_LAYERS.module_dict.items(),
+        mmcv.cnn.PLUGIN_LAYERS.module_dict.items(),
+    ):
+        ADAPTS.register(v, name=f'mmcv_{k}')
 except Exception:
     pass
