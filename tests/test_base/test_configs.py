ab31584084343e23b5c7df9c2eecc0afea12839a
LutingWang <wangluting.wlt@alibaba-inc.com>
2022-08-23 17:12:45 +0800

    fix(base): config cannot inherit UserDict

--- tests/test_base/test_configs.py
+++ tests/test_base/test_configs.py
@@ -1,4 +1,6 @@
+import copy
 import pathlib
+import pickle
 import tempfile
 
 import pytest
@@ -12,9 +14,17 @@ class TestConfigs:
     def config(self) -> Config:
         return Config(a=1)
 
-    def test_setitem(self, config: Config) -> None:
+    def test_len(self, config: Config) -> None:
+        assert len(config) == 1
+
+    def test_items(self, config: Config) -> None:
+        assert config['a'] == 1
+
         config['b'] = dict(c=3)
-        assert isinstance(config['b'], Config)
+        assert config['b'] == Config(c=3)
+
+        del config['b']
+        assert 'b' not in config
 
     def test_attrs(self, config: Config) -> None:
         assert config.a == 1
@@ -28,13 +38,21 @@ class TestConfigs:
         del config.b
         assert 'b' not in config
 
-        config.__dict__['_privilege'] = True
+    def test_copy(self, config: Config) -> None:
+        config.b = dict(c=3)
 
-        config.b = 2
-        assert 'b' in config.__dict__
+        cc = copy.copy(config)
+        cc.b.c = 'c'
+        assert config.b.c == 'c'  # type: ignore[attr-defined]
 
-        del config.b
-        assert 'b' not in config.__dict__
+        dcc = copy.deepcopy(config)
+        dcc.b.c = 3
+        assert config.b.c == 'c'  # type: ignore[attr-defined]
+
+    def test_pickle(self, config: Config) -> None:
+        tmp1 = pickle.dumps(config)
+        tmp2 = pickle.loads(tmp1)
+        assert tmp2 == config
 
     def test_merge(self, config: Config) -> None:
         assert Config.merge(1, 2) == 2
@@ -51,6 +69,9 @@ class TestConfigs:
             == Config(a=1, b=dict(c='c'))
 
     def test_load(self, data_dir: pathlib.Path) -> None:
+        assert Config.loads('a = 1\nb = dict(c=3)') == Config(a=1, b=dict(c=3))
+        assert Config.loads('a = 1\nb = dict(c=v)', globals=dict(v=3)) \
+            == Config(a=1, b=dict(c=3))
         assert Config.load(data_dir / 'config.py') == Config(a=1, b=dict(c=3))
 
     def test_dump(self) -> None:
--- todd/base/configs.py
+++ todd/base/configs.py
@@ -7,27 +7,64 @@ __all__ = [
     'Config',
 ]
 
+import copy
 import pathlib
-from collections import UserDict
 from functools import reduce
-from typing import Iterable, Mapping, Optional
+from typing import (
+    Any,
+    Dict,
+    Iterable,
+    Iterator,
+    Mapping,
+    MutableMapping,
+    NoReturn,
+    Optional,
+    TypeVar,
+)
 
 import yapf.yapflib.yapf_api as yapf
+
+T = TypeVar('T', bound='Config')
 
 BASE = '_base_'
 DELETE = '_delete_'
 
 
-class Config(UserDict):
+# Do not inherit `UserDict`. Otherwise, config cannot include `data` entry.
+class Config(MutableMapping):
+    _data: dict
 
     def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.__dict__['_privilege'] = False
+        self.__dict__.update(_data=dict())
+        self.update(*args, **kwargs)
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __missing__(self, key) -> NoReturn:
+        raise KeyError(key)
+
+    def __getitem__(self, key):
+        if key in self._data:
+            return self._data[key]
+        return self.__missing__(key)
 
     def __setitem__(self, key, item) -> None:
         if isinstance(item, Mapping):
             item = self.__class__(item)
-        super().__setitem__(key, item)
+        self._data[key] = item
+
+    def __delitem__(self, key) -> None:
+        del self._data[key]
+
+    def __iter__(self) -> Iterator:
+        return iter(self._data)
+
+    def __contains__(self, key) -> bool:
+        return key in self._data
+
+    def __repr__(self) -> str:
+        return repr(self._data)
 
     def __getattr__(self, name: str):
         if name in self:
@@ -35,20 +72,34 @@ class Config(UserDict):
         raise AttributeError(name)
 
     def __setattr__(self, name: str, value) -> None:
-        if self.has_privilege or name in self.__dict__:
+        if name in self.__dict__:
             super().__setattr__(name, value)
         else:
             self[name] = value
 
     def __delattr__(self, name: str) -> None:
-        if self.has_privilege:
-            super().__delattr__(name)
-        else:
-            del self[name]
+        del self[name]
 
-    @property
-    def has_privilege(self) -> bool:
-        return self.__dict__.get('_privilege', True)
+    def __copy__(self: T) -> T:
+        inst = self.__class__.__new__(self.__class__)
+        inst.__dict__.update(self.__dict__)
+        inst._data = self._data.copy()
+        return inst
+
+    def __deepcopy__(self: T, memo: Dict[int, Any]) -> T:
+        inst = self.__class__.__new__(self.__class__)
+        memo[id(self)] = inst
+        inst.__dict__.update({
+            k: copy.deepcopy(v, memo)
+            for k, v in self.__dict__.items()
+        })
+        return inst
+
+    def __getstate__(self) -> dict:
+        return self._data
+
+    def __setstate__(self, state: dict) -> None:
+        self.__dict__.update(_data=state)
 
     @classmethod
     def merge(cls, a, b):
@@ -88,6 +139,9 @@ class Config(UserDict):
         ]
         configs.append(config)
         return reduce(cls.merge, configs)
+
+    def copy(self: T) -> T:
+        return self.__copy__()
 
     def dumps(self) -> str:
 
