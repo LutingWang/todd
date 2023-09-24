a50d9236be99875fd5fcba455ab375de31b097a2
LutingWang <2457348692@qq.com>
2023-09-24 22:31:05 +0800

    fix(configs): fix for yapf

--- todd/base/configs.py
+++ todd/base/configs.py
@@ -53,6 +53,19 @@ class AttrDict(UserDict):
             raise AttributeError(e)
 
 
+class _import_:
+
+    def __init__(self, name: str) -> None:
+        self.__name = name
+        self.__module = importlib.import_module(name)
+
+    def __getattr__(self, attr: str):
+        return getattr(self.__module, attr)
+
+    def __repr__(self) -> str:
+        return f"_import_({repr(self.__name)})"
+
+
 class Config(AttrDict, dict):  # type: ignore[misc]
 
     def __setitem__(self, name: str, value) -> None:
@@ -111,19 +124,6 @@ class Config(AttrDict, dict):  # type: ignore[misc]
 
     @classmethod
     def load(cls, file) -> Self:
-
-        class _import_:
-
-            def __init__(self, name: str) -> None:
-                self.__name = name
-                self.__module = importlib.import_module(name)
-
-            def __getattr__(self, attr: str):
-                return getattr(self.__module, attr)
-
-            def __repr__(self) -> str:
-                return f"_import_({repr(self.__name)})"
-
         file = pathlib.Path(file)
         config = exec_(  # do not use `loads`
             file.read_text(),
@@ -161,10 +161,15 @@ class Config(AttrDict, dict):  # type: ignore[misc]
             l = 'mn'
             <BLANKLINE>
         """
-        code, _ = yapf.FormatCode(
-            '\n'.join(f'{k}={repr(self[k])}' for k in sorted(self)),
-            verify=True,
-        )
+        imports: list[str] = []
+        others: list[str] = []
+        for k in sorted(self):
+            v = self[k]
+            items = imports if isinstance(v, _import_) else others
+            item = f'{k}={repr(v)}'
+            items.append(item)
+        items = imports + others
+        code, _ = yapf.FormatCode('\n'.join(items))
         return code
 
     def dump(self, file) -> None:
