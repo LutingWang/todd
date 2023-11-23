2ad606aeabefb0e02bcbcb93046060250e092110
LutingWang <2457348692@qq.com>
2023-11-23 20:44:12 +0800

    fix(runners): check callback

--- todd/runners/callbacks/check.py
+++ todd/runners/callbacks/check.py
@@ -1,15 +1,21 @@
+__all__ = [
+    'CheckCallback',
+]
+
+from ...base import CallbackRegistry
 from ..types import Memo
 from .base import BaseCallback
 
 # TODO: check if the model has grad
 
 
+@CallbackRegistry.register_()
 class CheckCallback(BaseCallback):
 
     def before_run(self, memo: Memo) -> None:
         requires_grad_parameters = [
             name for name, parameter in
-            self.trainer.strategy.model.named_parameters()
+            self.trainer.strategy.module.named_parameters()
             if parameter.requires_grad
         ]
         self.trainer.logger.info(
@@ -19,7 +25,7 @@ class CheckCallback(BaseCallback):
 
         training_modules = [
             name
-            for name, module in self.trainer.strategy.model.named_modules()
+            for name, module in self.trainer.strategy.module.named_modules()
             if module.training
         ]
         self.trainer.logger.info(
