26d559d7e2610855bf5a7ba5c7c4d86212790c7b
LutingWang <2457348692@qq.com>
2023-12-14 17:27:26 +0800

    feat: remove support for strategy with runner

--- todd/runners/strategies/base.py
+++ todd/runners/strategies/base.py
@@ -2,7 +2,7 @@ __all__ = [
     'BaseStrategy',
 ]
 
-from typing import TYPE_CHECKING, Any, Callable, Mapping
+from typing import Any, Mapping
 
 import torch
 from torch import nn
@@ -10,9 +10,6 @@ from torch import nn
 from ...base import Config, ModelRegistry, OptimizerRegistry, StrategyRegistry
 from ...utils import StateDictMixin, get_rank
 from ..utils import RunnerHolderMixin
-
-if TYPE_CHECKING:
-    from ..base import BaseRunner
 
 
 @StrategyRegistry.register_()
@@ -29,12 +26,7 @@ class BaseStrategy(RunnerHolderMixin, StateDictMixin):
         self._build_model(model)
 
     def _build_model(self, config: Config) -> None:
-        model = ModelRegistry.build(config)
-        with_runner: Callable[[BaseRunner], None] | None = \
-            getattr(model, 'with_runner', None)
-        if with_runner is not None:
-            with_runner(self._runner)
-        self._model = model
+        self._model = ModelRegistry.build(config)
 
     def build_optimizer(self, config: Config) -> torch.optim.Optimizer:
         return OptimizerRegistry.build(config, model=self.module)
