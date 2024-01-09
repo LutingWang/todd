f333b6042bc3a9c4dd8d4a9548db7b281abf8488
LutingWang <2457348692@qq.com>
2024-01-09 17:35:16 +0800

    feat: torch module output shape calculator

--- todd/utils/torch.py
+++ todd/utils/torch.py
@@ -4,14 +4,17 @@ __all__ = [
     'get_world_size',
     'all_gather',
     'all_gather_',
+    'Shape',
 ]
 
 import functools
+import itertools
 import operator
 import os
 
 import torch
 import torch.distributed as dist
+from torch import nn
 
 
 def get_rank(*args, **kwargs) -> int:
@@ -82,3 +85,45 @@ def all_gather_(
         for shape, numel, container in zip(shapes, numel_list, containers)
     ]
     return tensors
+
+
+class Shape:
+
+    @classmethod
+    def module(cls, module: nn.Module, x: torch.Tensor) -> tuple[int, ...]:
+        if isinstance(module, (nn.Conv1d, nn.Conv2d, nn.Conv3d)):
+            return cls.conv(module, x)
+        raise TypeError(f"Unknown type {type(module)}")
+
+    @staticmethod
+    def _conv(
+        x: int,
+        padding: int,
+        dilation: int,
+        kernel_size: int,
+        stride: int,
+    ) -> int:
+        x += 2 * padding - dilation * (kernel_size - 1) - 1
+        return x // stride + 1
+
+    @classmethod
+    def conv(
+        cls,
+        module: nn.Conv1d | nn.Conv2d | nn.Conv3d,
+        x: torch.Tensor,
+    ) -> tuple[int, ...]:
+        b, c, *shape = x.shape
+
+        assert c == module.in_channels
+        c = module.out_channels
+
+        return b, c, *itertools.starmap(
+            cls._conv,
+            zip(
+                shape,
+                module.padding,
+                module.dilation,
+                module.kernel_size,
+                module.stride,
+            ),
+        )
