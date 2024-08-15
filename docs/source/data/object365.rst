827c54ad2ac2b80d1804dea5af7114f4a51a00b6
wangluting.wlt <wangluting.wlt@bytedance.com>
2024-08-15 22:37:39 +0800

    feat: object365

--- docs/source/data/object365.rst
+++ docs/source/data/object365.rst
@@ -3,49 +3,67 @@ Object365
 
 https://www.objects365.org/
 
+.. code-block:: bash
+
+    root=data/object365
+    mkdir -p ${root} && cd ${root}
+        mkdir annotations train val
+    cd ../..
+
+    base_url=https://dorc.ks3-cn-beijing.ksyun.com/data-set/2020Objects365%E6%95%B0%E6%8D%AE%E9%9B%86
+
+    download() {
+        local url=$1
+        local i=$2
+
+        patch=patch${i}.tar.gz
+        wget ${url}/${patch}
+        tar -zxf ${patch}
+    }
+
+.. code::
+
+    data/object365/
+
+Object365 v1
+------------
+
+.. code-block:: bash
+
+    cd ${root}
+        train_v1=train/v1
+        mkdir ${train_v1} && cd ${train_v1}
+            for i in {0..15}; do download ${base_url}/train ${i}; done
+        cd ../..
+
+        val_v1=val/v1
+        mkdir ${val_v1} && cd ${val_v1}
+            for i in {0..15}; do download ${base_url}/val/images/v1 ${i}; done
+        cd ../..
+    cd ../..
+
 Object365 v2
 ------------
 
 .. code-block:: bash
 
-    root=data/object365
-    mkdir -p ${root} && cd ${root}
+    cd ${root}
+        cd annotations
+            annotations_train_v2=zhiyuan_objv2_train.tar.gz
+            wget ${base_url}/train/${annotations_train_v2}
+            tar -zxf ${annotations_train_v2}
 
-    mkdir annotations train val
+            annotations_val_v2=zhiyuan_objv2_val.json
+            wget ${base_url}/val/${annotations_val_v2}
+        cd ..
 
-    base_url="https://dorc.ks3-cn-beijing.ksyun.com/data-set/2020Objects365%E6%95%B0%E6%8D%AE%E9%9B%86"
+        train_v2=train/v2
+        mkdir ${train_v2} && cd ${train_v2}
+            for i in {16..50}; do download ${base_url}/train ${i}; done
+        cd ../..
 
-    cd annotations
-        annotations_train=zhiyuan_objv2_train.tar.gz
-        wget ${base_url}/train/${annotations_train}
-        tar -zxf ${annotations_train}
-
-        wget ${base_url}/val/zhiyuan_objv2_val.json
-    cd ..
-
-    cd train
-        for i in {0..50}; do
-            patch=patch${i}.tar.gz
-            wget ${base_url}/train/${patch}
-            tar -zxf ${patch}
-        done
-    cd ..
-
-    cd val
-        for i in {0..15}; do
-            patch=patch${i}.tar.gz
-            wget ${base_url}/val/images/v1/${patch}
-            tar -zxf ${patch}
-        done
-        for i in {16..43}; do
-            patch=patch${i}.tar.gz
-            wget ${base_url}/val/images/v2/${patch}
-            tar -zxf ${patch}
-        done
-    cd ..
-
+        val_v2=val/v2
+        mkdir ${val_v2} && cd ${val_v2}
+            for i in {16..43}; do download ${base_url}/val/images/v2 ${i}; done
+        cd ../..
     cd ../..
-
-.. code::
-
-    data/v3det/
--- todd/datasets/object365.py
+++ todd/datasets/object365.py
@@ -1,23 +1,30 @@
 # pylint: disable=duplicate-code
 
 __all__ = [
-    'LVISDataset',
+    'Object365Dataset',
 ]
 
 import pathlib
 from collections import UserList
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Literal, Mapping, TypedDict, cast
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Iterable,
+    Literal,
+    Mapping,
+    TypedDict,
+    cast,
+)
 from typing_extensions import Self
 
 import torch
-from lvis import LVIS
-from lvis.boundary_utils import ann_to_mask
+from pycocotools.coco import COCO
 
 from ..registries import DatasetRegistry
 from .access_layers import PILAccessLayer
 from .coco import BaseDataset as BaseCOCODataset
-from .coco import BaseKeys as COCOKeys
+from .coco import Keys as COCOKeys
 
 if TYPE_CHECKING:
     from todd.tasks.object_detection import BBox, FlattenBBoxesXYWH
@@ -27,35 +34,43 @@ Split = Literal['train', 'val']
 
 class Keys(COCOKeys):
 
-    def __init__(self, lvis: LVIS, *args, **kwargs) -> None:
-        self._lvis = lvis
-        super().__init__(lvis.get_img_ids(), *args, **kwargs)
+    def __init__(self, *args, ignore: Iterable[str], **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        ignore = set(ignore)
+        if len(ignore) > 0:
+            ignore_indices = [i for i in range(len(self)) if self[i] in ignore]
+            assert len(ignore_indices) == len(ignore)
+            for i in ignore_indices[::-1]:
+                self._image_ids.pop(i)
 
     def _getitem(self, image_id: int) -> str:
-        image, = self._lvis.load_imgs([image_id])
-        url: str = image['coco_url']
-        return url.removeprefix('http://images.cocodataset.org/')
+        item = super()._getitem(image_id)
+        return item.removeprefix('images/')
 
 
 @dataclass(frozen=True)
 class Annotation:
     area: float
-    mask: torch.Tensor
     bbox: 'BBox'
     category: int
+    is_crowd: bool
+    is_fake: bool
+    is_reflected: bool
 
     @classmethod
     def load(
         cls,
-        lvis: LVIS,
         annotation: Mapping[str, Any],
         categories: Mapping[int, int],
     ) -> Self:
         return cls(
             annotation['area'],
-            torch.from_numpy(ann_to_mask(annotation, lvis.imgs)),
             cast('BBox', annotation['bbox']),
             categories[annotation['category_id']],
+            bool(annotation['iscrowd']),
+            bool(annotation['isfake']),
+            bool(annotation['isreflected']),
         )
 
 
@@ -64,24 +79,20 @@ class Annotations(UserList[Annotation]):
     @classmethod
     def load(
         cls,
-        lvis: LVIS,
+        coco: COCO,
         image_id: int,
         categories: Mapping[int, int],
     ) -> Self:
-        annotation_ids = lvis.get_ann_ids([image_id])
-        annotations = lvis.load_anns(annotation_ids)
+        annotation_ids = coco.getAnnIds([image_id])
+        annotations = coco.loadAnns(annotation_ids)
         return cls(
-            Annotation.load(lvis, annotation, categories)
+            Annotation.load(annotation, categories)
             for annotation in annotations
         )
 
     @property
     def areas(self) -> torch.Tensor:
         return torch.tensor([annotation.area for annotation in self])
-
-    @property
-    def masks(self) -> torch.Tensor:
-        return torch.stack([annotation.mask for annotation in self])
 
     @property
     def bboxes(self) -> 'FlattenBBoxesXYWH':
@@ -96,6 +107,18 @@ class Annotations(UserList[Annotation]):
     def categories(self) -> torch.Tensor:
         return torch.tensor([annotation.category for annotation in self])
 
+    @property
+    def is_crowd(self) -> torch.Tensor:
+        return torch.tensor([annotation.is_crowd for annotation in self])
+
+    @property
+    def is_fake(self) -> torch.Tensor:
+        return torch.tensor([annotation.is_fake for annotation in self])
+
+    @property
+    def is_reflected(self) -> torch.Tensor:
+        return torch.tensor([annotation.is_reflected for annotation in self])
+
 
 class T(TypedDict):
     id_: str
@@ -104,42 +127,58 @@ class T(TypedDict):
 
 
 @DatasetRegistry.register_()
-class LVISDataset(BaseCOCODataset[LVIS, T]):
+class Object365Dataset(BaseCOCODataset[COCO, T]):
     _keys: Keys
 
-    DATA_ROOT = pathlib.Path('data/lvis')
+    DATA_ROOT = pathlib.Path('data/object365')
     ANNOTATIONS_ROOT = DATA_ROOT / 'annotations'
+
+    IGNORE_KEYS = (
+        'v1/patch6/objects365_v1_00320532',
+        'v1/patch6/objects365_v1_00320534',
+        'v2/patch16/objects365_v2_00908726',
+    )
 
     def __init__(
         self,
         *args,
         split: Split,
-        version: Literal['v0.5', 'v1'] = 'v1',
+        version: Literal['v1', 'v2'] = 'v2',
         access_layer: PILAccessLayer | None = None,
         annotations_file: pathlib.Path | str | None = None,
         **kwargs,
     ) -> None:
+        self._split = split
+
         if access_layer is None:
             access_layer = PILAccessLayer(
                 data_root=str(self.DATA_ROOT),
+                task_name=split,
                 suffix=self.SUFFIX,
             )
         if annotations_file is None:
             annotations_file = (
-                self.ANNOTATIONS_ROOT / f'lvis_{version}_{split}.json'
+                self.ANNOTATIONS_ROOT / f'zhiyuan_obj{version}_{split}.json'
             )
 
-        lvis = LVIS(annotations_file)
+        coco = COCO(annotations_file)
 
         self._categories = {
             category_id: i
-            for i, category_id in enumerate(lvis.get_cat_ids())
+            for i, category_id in enumerate(coco.getCatIds())
         }
 
-        super().__init__(*args, api=lvis, access_layer=access_layer, **kwargs)
+        super().__init__(*args, api=coco, access_layer=access_layer, **kwargs)
 
     def build_keys(self) -> Keys:
-        return Keys(self._api, self.SUFFIX)
+        ignore_keys: Iterable[str]
+        if self._split == 'train':
+            ignore_keys = self.IGNORE_KEYS
+        elif self._split == 'val':
+            ignore_keys = tuple()
+        else:
+            raise ValueError(f"Unknown split: {self._split}")
+        return Keys(self._api, self.SUFFIX, ignore=ignore_keys)
 
     def __getitem__(self, index: int) -> T:
         key, image = self._access(index)
