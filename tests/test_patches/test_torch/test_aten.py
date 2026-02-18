e2c7fb7beedbebfaa0c4f7ddb9fb73a11a1affd7
LutingWang <2457348692@qq.com>
2026-02-18 16:49:12 +0800

    ci: fix random int test

--- tests/test_patches/test_torch/test_aten.py
+++ tests/test_patches/test_torch/test_aten.py
@@ -4,5 +4,5 @@ from todd.utils import init_seed
 
 def test_random_int() -> None:
     init_seed(42)
-    assert random_int() == 534895718
     assert random_int() == 199900595
+    assert random_int() == 787846414
