930fb195954a3438871dcdeacdf9a2d51c0dcd74
LutingWang <2457348692@qq.com>
2026-09-01 16:22:08 +0800

    bump: version 0.7.0 → 0.7.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ project = 'todd'
 project_copyright = '2022, Luting Wang'
 author = 'Luting Wang'
 
-release = '0.7.0'
+release = '0.7.1'
 
 extensions = [
     'sphinx.ext.autodoc',
--- todd/src/todd/__init__.py
+++ todd/src/todd/__init__.py
@@ -1,6 +1,6 @@
 """Core utilities for Toolkit for Object Detection Distillation."""
 
-__version__ = '0.7.0'
+__version__ = '0.7.1'
 
 from . import bases, colors, configs, loggers, patches, registries, utils
 from .bases.configs import Config
