05474a857af49e8f3782faaa68bd2c5e35fd25ea
LutingWang <2457348692@qq.com>
2026-09-01 10:39:24 +0800

    feat: remove deprecated dataset files and add merge LMDBs script

---
+++ todd_torch/datasets/__init__.py
@@ -0,0 +1,6 @@
+from . import access_layers
+from .base import *
+from .constants import *
+from .index import *
+from .pil import *
+from .registries import *
--- todd_torch/scripts/merge_lmdbs.py
+++ todd_torch/scripts/merge_lmdbs.py
@@ -1,3 +1,5 @@
+#!python3
+
 import argparse
 
 import lmdb
